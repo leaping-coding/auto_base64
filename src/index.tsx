a1b1377f51a0e609fdc2f35a801446af2a0fbb4c
李平 <liping@xbonline.net>
2024-11-11 10:40:39 +0800

    feat: 实现自动拼接参数并执行base64编码

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import ReactDOM from 'react-dom/client'
-import { bitable, IAttachmentField } from '@lark-base-open/js-sdk';
-import { Alert, AlertProps } from 'antd';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import { bitable, IOpenTextSegment } from "@lark-base-open/js-sdk";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <LoadApp/>
+    <LoadApp />
   </React.StrictMode>
-)
+);
 
 function LoadApp() {
-  const [info, setInfo] = useState('get table name, please waiting ....');
-  const [alertType, setAlertType] = useState<AlertProps['type']>('info');
+  const [info, setInfo] = useState("正在计算中...");
   useEffect(() => {
     const fn = async () => {
+      setInfo("正在计算中...");
       const table = await bitable.base.getActiveTable();
-      const tableName = await table.getName();
-      setInfo(`The table Name is ${tableName}`);
-      setAlertType('success');
+      const fieldIdList = await table.getFieldMetaList();
+      const recordNames = fieldIdList.map((field) => field.name);
+      const paramsFieldId =
+        fieldIdList.find((field) => field.name == "params")?.id ?? "";
+      //如果没有params字段，则需要创建
+
+      const rowRecords = await table.getRecords({
+        pageSize: 5000,
+      });
+      let paramsStr = "";
+      console.log(rowRecords.records.length);
+      for (const record of rowRecords.records) {
+        paramsStr = "";
+        let keys = Object.keys(record.fields);
+        let values = Object.values(record.fields);
+        for (let i = 0; i < values.length; i++) {
+          if (values[i] !== null) {
+            const filed = await table.getField(keys[i]);
+            const name = await filed.getName();
+            if (name != "id" && name != "params") {
+              paramsStr += `&${name}=${
+                (values[i] as unknown as IOpenTextSegment[])[0]?.text
+              }`;
+            }
+          }
+        }
+        if (paramsStr) {
+          paramsStr = paramsStr.slice(1);
+          table.setCellValue(paramsFieldId, record.recordId, btoa(paramsStr));
+        }
+      }
+      setInfo("计算完成");
     };
     fn();
   }, []);
 
-  return <div>
-    <Alert message={info} type={alertType} />
-  </div>
+  return (
+    <div>
+      <p>{info}</p>
+    </div>
+  );
 }
